0910250a082bfdb36dcfda750a6cc394927fd271
Cedric David <cedric.david@btopenworld.com>
2018-08-27 12:19:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,17 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('.tasty');
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
+   getIt()
 
 });
